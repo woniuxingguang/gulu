ae1dde7bb31db1c888f590d8e13baab9be5fc490
shan123456 <757446629@qq.com>
2020-10-21 11:08:41 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,45 +1,16 @@
 <template>
+<div>Button 示例</div>
+<h1>示例1</h1>
 <div>
-    Button文档
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
-    <p>button文档</p>
+    <Button>你好</Button>
 </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+    components: {
+        Button
+    }
+}
+</script>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -89,6 +89,7 @@ aside {
     left: 0;
     padding-top: 70px;
     height: 100%;
+    z-index: 8;
 
     >h2 {
         margin-bottom: 4px;
