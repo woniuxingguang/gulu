33ba802b089d6553c78c16c8ba632cd99ac14c97
shan123456 <757446629@qq.com>
2020-10-21 10:53:58 +0800

    优化switch css

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <Switch v-model:value="y" />
+    <Switch v-model:value="bool" />
 </div>
 </template>
 
@@ -14,9 +14,9 @@ export default {
         Switch
     },
     setup() {
-        const y = ref(false)
+        const bool = ref(false)
         return {
-            y
+            bool
         }
     }
 }
