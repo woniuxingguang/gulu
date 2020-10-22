8b34d38c6806f84795b653a53e35d18aacd1bd86
shan123456 <757446629@qq.com>
2020-10-22 16:48:45 +0800

    Dialog 支持点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
-<div class="gulu-dialog-overlay"></div>
-<div class="gulu-dialog-wrapper">
-    <div class="gulu-dialog">
-        <header>
-            标题
-            <span class="gulu-dialog-close"></span>
-        </header>
-        <main>
-            <p>第一行字</p>
-            <p>第二行字</p>
-        </main>
-        <footer>
-            <Button level="main">取消</Button>
-            <Button>确定</Button>
-        </footer>
+<template v-if="visible">
+    <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="gulu-dialog-wrapper">
+        <div class="gulu-dialog">
+            <header>
+                标题
+                <span @click="close" class="gulu-dialog-close"></span>
+            </header>
+            <main>
+                <p>第一行字</p>
+                <p>第二行字</p>
+            </main>
+            <footer>
+                <Button level="main" @click="ok">Ok</Button>
+                <Button @click="cancel">Cancel</Button>
+            </footer>
+        </div>
     </div>
-</div>
+</template>
 </template>
 
 <script lang="ts">
@@ -23,6 +25,49 @@ import Button from './Button.vue'
 export default {
     components: {
         Button
+    },
+    props: {
+        visible: {
+            type: Boolean,
+            default: false
+        },
+        closeOnClickOverlay: {
+            type: Boolean,
+            default: true
+        },
+        ok: {
+            type: Function
+        },
+        cancel: {
+            type: Function
+        }
+    },
+    setup(props, context) {
+        const close = () => {
+            context.emit('update:visible', false)
+        }
+        const onClickOverlay = () => {
+            if (props.closeOnClickOverlay) {
+                close()
+            }
+        }
+        const ok = () => {
+            // if (props.ok && props.ok() !== false) {
+            //     close()
+            // }
+            if (props.ok?.() !== false) {
+                close()
+            }
+        }
+        const cancel = () => {
+            close()
+        }
+        return {
+            close,
+            onClickOverlay,
+            ok,
+            cancel
+        }
     }
 }
 </script>
