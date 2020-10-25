a879059f1d5574d66fd9b9f04610dd91c062ba69
shan123456 <757446629@qq.com>
2020-10-25 10:33:52 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,7 +2,7 @@
 <div>
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav" ref="container">
-            <div class="gulu-tabs-nav-item" :class="{selected: t == selected}" v-for="(t,index) in titles" :key="index" :ref="el => { if (el) navItems[index] = el}" @click="select(t)">{{t}}</div>
+            <div class="gulu-tabs-nav-item" :class="{selected: t == selected}" v-for="(t,index) in titles" :key="index" :ref="el => { if (t == selected) selectedItem = el}" @click="select(t)">{{t}}</div>
             <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
         </div>
         <div class="gulu-tabs-content">
@@ -28,16 +28,14 @@ export default {
         }
     },
     setup(props, context) {
-        const navItems = ref < HTMLDivElement[] > ([])
+        const selectedItem = ref < HTMLDivElement > (null)
         const indicator = ref < HTMLDivElement > (null)
         const container = ref < HTMLDivElement > (null)
         const x = () => {
-            const divs = navItems.value
-            const result = divs.filter(div => div.classList.contains('selected'))[0]
-            // 获取宽度
+            // 获取 选中 宽度
             const {
                 width
-            } = result.getBoundingClientRect()
+            } = selectedItem.value.getBoundingClientRect()
             // 动态给导航 下边横线
             indicator.value.style.width = width + 'px'
             // container 的left坐标
@@ -47,7 +45,7 @@ export default {
             //获取当前 选中 nav 的left坐标
             const {
                 left: left2
-            } = result.getBoundingClientRect()
+            } = selectedItem.value.getBoundingClientRect()
             const left = left2 - left1
             // 动态 indicator 的位置
             indicator.value.style.left = left + 'px'
@@ -79,7 +77,7 @@ export default {
             titles,
             current,
             select,
-            navItems,
+            selectedItem,
             indicator,
             container
         }
