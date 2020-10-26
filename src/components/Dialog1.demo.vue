8aa09f07ad41d59ef5698baf1a384678c71a8fdc
shan123456 <757446629@qq.com>
2020-10-26 17:47:29 +0800

    改造 Dialog demo

---
+++ src/components/Dialog1.demo.vue
@@ -0,0 +1,52 @@
+<demo>
+    常规使用
+</demo>
+
+<template>
+<div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+        <template v-slot:content>
+            <div>你好</div>
+            <div>hi</div>
+        </template>
+        <template v-slot:title>
+            <strong>加粗的标题</strong>
+        </template>
+    </Dialog>
+</div>
+</template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {
+    ref
+} from 'vue'
+import {
+    openDialog
+} from '../lib/openDialog'
+
+export default {
+    components: {
+        Dialog,
+        Button
+    },
+    setup() {
+        const x = ref(false)
+        const toggle = () => {
+            x.value = !x.value
+        }
+        const f1 = () => {
+            return false
+        }
+        const f2 = () => {}
+        return {
+            x,
+            toggle,
+            f1,
+            f2
+        }
+    }
+}
+</script>
---
+++ src/components/Dialog2.demo.vue
@@ -0,0 +1,43 @@
+<demo>
+    一键打开 Dailog
+</demo>
+
+<template>
+<div>
+    <Button @click="showDialog">show</BUtton>
+</div>
+</template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+import {
+    ref,
+    h
+} from 'vue'
+import {
+    openDialog
+} from '../lib/openDialog'
+
+export default {
+    components: {
+        Button
+    },
+    setup() {
+        const showDialog = () => {
+            openDialog({
+                title: h('strong', {}, '标题'),
+                content: '你好',
+                ok() {
+                    console.log('ok')
+                },
+                cancel() {
+                    console.log('cancel')
+                }
+            })
+        }
+        return {
+            showDialog
+        }
+    }
+}
+</script>
